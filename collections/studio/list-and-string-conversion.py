27a1d8f0b3dcb27f1d9edfafd05fcde05eb8257c
a-s-h-l-e-y <ash.smith.anne@gmail.com>
2024-08-27 21:47:22 -0500

    Studio Exercises

--- collections/studio/list-and-string-conversion.py
+++ collections/studio/list-and-string-conversion.py
@@ -7,17 +7,47 @@ strings = [proto_list1, proto_list2, proto_list3, proto_list4]
 
 # a) Use the 'in' method to check to see if the words in each string are separated by commas (,), semicolons (;) or just spaces.
 
+list_array = proto_list1.split(",")
+#for word in list_array:
+#    print(word)
+
+
 
 # b) If the string uses commas to separate the words, split it into an array, reverse the entries, and then join the array into a new comma separated string.
 
 
+# using join() + reversed() + split()
+# Reverse string split
+final_string =  ", ".join(reversed(proto_list1.split(",")))
+ 
+# print result
+print("The string after reverse split is: " + str(final_string))
+
 
 # c) If the string uses semicolons to separate the words, split it into an array, alphabetize the entries, and then join the array into a new comma separated string.
 
+final_string2 =  "-".join(sorted(proto_list2.split(";")))
 
+# print result
+print("The string after alphabetized split is: " + str(final_string2))
 
 # d) If the string uses spaces to separate the words, split it into an array, reverse alphabetize the entries, and then join the array into a new space separated string.
 
+final_string3 =  " ".join(sorted(proto_list3.split(" "), reverse=True))
 
+
+# print result
+print("The string after reverse split is: " + str(final_string3))
 
 # e) If the string uses ‘comma spaces’ to separate the list, modify your code to produce the same result as part “b”, making sure that the extra spaces are NOT part of the final string.
+
+#Remove the white space from the string before you split it.
+
+final_string4_list = proto_list4.replace(' ','').split(',')
+
+# using join() + reversed() + split()
+# Reverse string split
+final_string4_list =  ",".join(reversed(proto_list4.split(",")))
+ 
+# print result
+print("The string after reverse split is: " + str(final_string4_list))
--- collections/studio/string-modification.py
+++ collections/studio/string-modification.py
@@ -2,14 +2,29 @@ my_string = "LaunchCode"
 
 
 # a) Use string methods to remove the first three characters from the string and add them to the end.
+my_string_slice = my_string[:3]
 
+print(my_string_slice)
 
+my_string_slice_NEW = my_string[3:10] + my_string_slice
 
 # Use a template literal to print the original and modified string in a descriptive phrase.
+
+print(f"The word {my_string} in pig latin is {my_string_slice_NEW}")
 
 
 
 # b) Modify your code to accept user input. Query the user to enter the number of letters that will be relocated.
 
+number_of_letters = int(input("Enter the number of letters: "))
+
+#print(number_of_letters)
 
 # c) Add validation to your code to deal with user inputs that are longer than the word. In such cases, default to moving 3 characters. Also, the template literal should note the error.
+
+while number_of_letters > len(my_string):
+    print("Number Longer Than Word")
+    number_of_letters = 3
+
+# print(number_of_letters)
+
