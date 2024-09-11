dac32929362809b63f3eee84b514409ac7f15079
a-s-h-l-e-y <ash.smith.anne@gmail.com>
2024-09-10 21:46:35 -0500

    Studio File Answers

--- functions/studio/functions-studio.py
+++ functions/studio/functions-studio.py
@@ -1,18 +1,48 @@
 # We want to COMPLETELY reverse a list by flipping the order of the entries AND flipping the order of characters in each element.
+# e) Create a variable of type string to test your new function. 
+string_list = input("Enter a String: ")
 
 # a) Define a 'reverse_characters' function. Give it one parameter, which will be the string to reverse.
+def reverse_characters(string_list):
+    
 # b) Within the function, use the 'list' function to split a string into a list of individual characters
-# c) 'reverse' your new list.
-# d) Use 'join' to create the reversed string and return that string from the function.
-# e) Create a variable of type string to test your new function. # f) Use 'print(reverse_characters(my_variable_name))'; to call the function and verify that it correctly reverses the characters in the string.
+# using list() function to create a list
+    list1 = list(string_list)
+    list1.reverse() 
+
+    list_final=''.join(list1)
+
+    # print(list1)
+    # print(list_final)
+    # print(list1)       
+    #if string_list.isdigit():
+   
+
+    #if type(string_list) == str:
+# # c) 'reverse' your new list.
+#     list1.reverse()
+
+# # d) Use 'join' to create the reversed string and return that string from the function.
+    return list_final
+
+
+# f) Use 'print(reverse_characters(my_variable_name))'; to call the function and verify that it correctly reverses the characters in the string.
+print(reverse_characters(string_list))
+
 # g) Use method chaining to reduce the lines of code within the function.
 
-
-
 # 2) The 'split' method does not work on numbers, but we want the function to return a number with all the digits reversed (e.g. 1234 converts to 4321 and NOT the string "4321")
+
+# SEE ABOVE FUNCTION ADDITIONS
+
 # a) Add an if statement to your reverse_characters function to check the typeof the parameter.
 # b - d) If type is ‘string’, return the reversed string as before. If type is ‘number’, convert the parameter to a string, reverse the characters, then convert it back into a number. Return the reversed number.
+
+# SEE ABOVE FUNCTION ADDITIONS
+
 # e) Be sure to print the result returned by the function to verify that your code works for both strings and numbers. Do this before moving on to the next steps.
+
+# TEST INPUT WITH STRINGS AND NUMBERS
 
 # 3) Create a new function with one parameter, which is the list we want to change. The function should:
 # a) Define and initialize an empty list.
@@ -27,3 +57,4 @@
 list_test1 = ['apple', 'potato', 'Capitalized Words']
 list_test2 = [123, 8897, 42, 1168, 8675309]
 list_test3 = ['hello', 'world', 123, 'orange']
+
